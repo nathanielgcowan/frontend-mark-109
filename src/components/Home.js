df39aca5c4ded7a1e81c12b013d88c8b074a32d6
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2021-11-26 19:50:49 -0600

    work on pokedex

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,10 @@
-
+import React from 'react'
+import { Link } from 'react-router-dom';
 const Home = () => (
     <div>
         <p>
             <h1>Pokedex</h1>
+            <Link to="/mypokemon">See Pokemon</Link>
         </p>
     </div>
 );
